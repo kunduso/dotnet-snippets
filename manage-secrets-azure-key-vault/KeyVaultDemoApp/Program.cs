1da81d58a1ac24c8586b746d56ce0d3fc5941919
Sourav Kundu <sourav12kundu@gmail.com>
2021-10-07 06:07:12 -0500

    added capability to read secrets from azure key vault

---
+++ manage-secrets-azure-key-vault/KeyVaultDemoApp/Program.cs
@@ -0,0 +1,41 @@
+using System;
+using Azure.Identity;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace KeyVaultDemoApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    // Build the current set of configuration to load values from
+                    // JSON files and environment variables, including VaultName.
+                    var builtConfig = config.Build();
+
+                    // Use VaultName from the configuration to create the full vault URL.
+                    var vaultName = builtConfig["VaultName"];
+                    Uri vaultUri = new Uri($"https://{vaultName}.vault.azure.net/");
+
+                    // Load all secrets from the vault into configuration. This will automatically
+                    // authenticate to the vault using a managed identity. If a managed identity
+                    // is not available, it will check if Visual Studio and/or the Azure CLI are
+                    // installed locally and see if they are configured with credentials that can
+                    // access the vault.
+                    config.AddAzureKeyVault(vaultUri, new DefaultAzureCredential());
+                });
+    }
+}
